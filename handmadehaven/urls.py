59664646b93375f581faea4070d5e23cc9678543
JordanKingVeoxyl <veoxyl@gmail.com>
2023-10-27 17:14:43 +0000

    Configured Admin and added functionality to the blog

--- handmadehaven/urls.py
+++ handmadehaven/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from blog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('summernote/', include('django_summernote.urls')),
     path('', views.get_blog_list, name='get_blog_list'),
     path('add', views.add_item, name='add'),
     path('edit/<item_id>', views.edit_item, name='edit'),
